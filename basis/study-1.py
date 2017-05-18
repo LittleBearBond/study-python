e009932fc47a50f15dbd877962c0490d1a61a97e
xiongjian <xiongjian@didichuxing.com>
2017-05-18 22:33:11 +0800

    add fiels

--- basis/study-1.py
+++ basis/study-1.py
@@ -1,16 +1,19 @@
 
 import sys
 
-print (sys.platform)
-print (sys.api_version)
-print (sys.prefix)
-print (sys.stderr)
+# print (sys.platform)
+# print (sys.api_version)
+# print (sys.prefix)
+# print (sys.stderr)
 
-a = 100
-if a >= 0:
-    print(a)
-else:
-    print (-a)
+# a = 100
+# if a >= 0:
+#     print(a)
+# else:
+#     print (-a)
+
+#输出hello word
+print ("helle word")
 
 #name = input('please enter your name: ')
 #print('hello,', name)
