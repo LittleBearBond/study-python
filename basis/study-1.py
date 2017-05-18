9db7c1d173e01672e2ee6fdefae74a28f62a840a
xiongjian <xiongjian@didichuxing.com>
2017-05-18 17:16:56 +0800

    init add

---
+++ basis/study-1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding:utf-8
+import sys
+
+print (sys.platform)
+print (sys.api_version)
+print (sys.prefix)
+print (sys.stderr)
+
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print (-a)
+
+#name = input('please enter your name: ')
+#print('hello,', name)
